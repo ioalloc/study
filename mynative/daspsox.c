9599888d6a8dfdc2ec994e34dd72913aa7d144ca
RubyYavin <740150701@qq.com>
2015-02-06 12:01:33 +0800

    sox test successful! And the server echo right data

--- mynative/daspsox.c
+++ mynative/daspsox.c
@@ -29,17 +29,50 @@ typedef struct readComp
 	uint8_t		what;
 }readComp;
 
+DASP_STRUCT* createdasp(const SOX_STRUCT *sox)
+{
+	DASP_STRUCT *dasp;
+	dasp = malloc(sizeof(DASP_STRUCT));
+	dasp->sessionId = 0xffff;
+	dasp->seqNum = 1;
+	dasp->msgOpt &= 0x1<<4;
+	//dasp->msgOpt |= 0;
+	dasp->payload = sox;
+	return dasp;
+}
+
+SOX_STRUCT* createsox(uint8_t cmd,uint8_t replyNum,uint16_t componentId,uint8_t what)
+{
+	SOX_STRUCT *sox;
+	sox = malloc(sizeof(SOX_STRUCT));
+	readComp *rc;
+	rc = malloc(sizeof(readComp));
+	rc->componentId = componentId;
+	rc->what = what;
+	sox->cmd = cmd;
+	sox->replyNum = replyNum;
+	sox->payload = rc;
+	return sox;
+}
+
 int main(int argc, char const *argv[])
 {
 	int client_sockfd;
-	int len;
+	int len,sox_len=10;
 	struct sockaddr_in remote_addr; //服务器端网络地址结构体
 	int sin_size;
 	char buf[BUFSIZ];  //数据传送的缓冲区
+
+	DASP_STRUCT *dasp;
+	SOX_STRUCT *sox;
+
+	sox = createsox('c',1,12,'t');
+	dasp = createdasp(sox);
+
 	memset(&remote_addr,0,sizeof(remote_addr)); //数据初始化--清零
 	remote_addr.sin_family=AF_INET; //设置为IP通信
 	remote_addr.sin_addr.s_addr=inet_addr("127.0.0.1");//服务器IP地址
-	remote_addr.sin_port=htons(8080); //服务器端口号
+	remote_addr.sin_port=htons(1876); //服务器端口号
 
 	/*创建客户端套接字--IPv4协议，面向无连接通信，UDP协议*/
 	if((client_sockfd=socket(PF_INET,SOCK_DGRAM,0))<0)
@@ -52,7 +85,7 @@ int main(int argc, char const *argv[])
 	sin_size=sizeof(struct sockaddr_in);
 
 	/*向服务器发送数据包*/
-	if((len=sendto(client_sockfd,buf,strlen(buf),0,(struct sockaddr *)&remote_addr,sizeof(struct sockaddr)))<0)
+	if((len=sendto(client_sockfd,dasp,sox_len,0,(struct sockaddr *)&remote_addr,sizeof(struct sockaddr)))<0)
 	{
 		perror("recvfrom");
 		return 1;
