73fcb4a2cd6d3ec8f3e5e8e57a2f23866156254b
RubyYavin <740150701@qq.com>
2015-02-09 17:32:12 +0800

    test

--- zlog/test.c
+++ zlog/test.c
@@ -4,7 +4,7 @@
 int main(int argc,char **argv)
 {
 	int rc;
-	zlog_category_t *c;
+	zlog_category_t *c,*c2;
 	rc = zlog_init("./zlog.conf");
 	if(rc){
 		printf("zlog init failed!\n");
@@ -12,13 +12,14 @@ int main(int argc,char **argv)
 	}
 
 	c = zlog_get_category("my_cat");
-	if(!c){
+	c2 = zlog_get_category("my_cat2");
+	if(!c&&!c2){
 		printf("get cat failed!\n");
 		zlog_fini();
 		return -2;
 	}
 	zlog_info(c,"hello world,^_^o(∩∩)o...哈哈");
-
+	zlog_info(c2,"hello c2\n");
 	zlog_fini();
 
 	return 0;
